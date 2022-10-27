27a0e5212376f921eb80e95ce0c10b005e7987ee
Victoria <veejo90@gmail.com>
2022-10-27 11:19:52 -0400

    color change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-
-import './App.css';
-import Heading from './components/header';
-import Footer from './components/footer';
-import HomePage from './components/homepage';
+import "./App.css";
+import Heading from "./components/header";
+import Footer from "./components/footer";
+import HomePage from "./components/homepage";
 
 function App() {
   return (
-    <div>
+    <div className="w3-light-grey">
       <HomePage />
     </div>
   );
